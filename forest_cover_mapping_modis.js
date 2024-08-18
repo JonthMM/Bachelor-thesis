9aa180756dddc4d267ed3ac709fc4e9e95824798
Jonathan Wisser <jonathanmader16@gmail.com>
2024-08-18 23:40:25 +0200

    Update forest_cover_mapping_modis.js

--- forest_cover_mapping_modis.js
+++ forest_cover_mapping_modis.js
@@ -4,29 +4,32 @@ var landCover = ee.ImageCollection("MODIS/061/MCD12Q1")
                 .first()
                 .select('LC_Type1');
 
-// Auswahl von nur den fünf Waldklassen
-var classesToDisplay = [1, 2, 3, 4, 5];
+// Auswahl von nur den fünf Waldklassen und diesen, die 60+% tree cover haben
+var classesToDisplay = [1, 2, 3, 4, 5, 6, 8];
 
-// Maske weöche nur die fünf Waldklassen enthält
+// Maske welche nur die sieben ausgewählten Klassen enthält
 var maskedLandCover = landCover.updateMask(landCover.eq(1)
                             .or(landCover.eq(2))
                             .or(landCover.eq(3))
                             .or(landCover.eq(4))
-                            .or(landCover.eq(5)));
+                            .or(landCover.eq(5))
+                            .or(landCover.eq(6))
+                            .or(landCover.eq(8))
+                            );
 
 // Anzeigen der Ergebnisse auf der Karte mit den entsprechenden Farben
 Map.centerObject(maskedLandCover, 2.5); // Zoom auf Welt
 Map.addLayer(maskedLandCover, {
   min: 1,
-  max: 5,
-  palette: ['05450a', '086a10', '54a708', '78d203', '009900'] // Farben von https://developers.google.com/earth-engine/datasets/catalog/MODIS_061_MCD12Q1#bands
+  max: 8,
+  palette: ['05450a', '086a10', '54a708', '78d203', '009900', 'c6b044', 'dade48'] // Farben von https://developers.google.com/earth-engine/datasets/catalog/MODIS_061_MCD12Q1#bands
 }, 'All mayor Forest Classes');
 
 
-// Exportieren des GeoTiffs nach Drive
+// Exportieren des GeoTiffs nach Drive zum benutzen als Karte
 Export.image.toDrive({
   image: maskedLandCover,
-  description: 'forest_cover_export_world',  // Name der Datei
+  description: 'woody_and_forest_cover_export_world',  // Name der Datei
   folder: 'GEE_Exports',  // Name des Ordners in Google Drive (optional)
   scale: 500,  // Auflösung in Metern pro Pixel
   maxPixels: 10000000000000,  // Maximal zulässige Anzahl an Pixeln
@@ -57,12 +60,14 @@ function createLegend() {
   legend.add(legendTitle);
 
   // Farben und Labels der Attribute der Legende
-  var palette = ['#05450a', '#086a10', '#54a708', '#78d203', '#009900'];
+  var palette = ['#05450a', '#086a10', '#54a708', '#78d203', '#009900', '#c6b044', '#dade48'];
   var names = ['Evergreen Needleleaf Forests', 
                'Evergreen Broadleaf Forests', 
                'Deciduous Needleleaf Forests', 
                'Deciduous Broadleaf Forests', 
-               'Mixed Forests'];
+               'Mixed Forests',
+               'Closed Shrublands',
+               'Woody Savannas'];
   
   // Größen und Farbenspezifikation für die Legende selbst
   for (var i = 0; i < palette.length; i++) {
